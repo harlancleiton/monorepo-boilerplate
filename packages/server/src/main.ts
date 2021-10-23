e7748022b953ba7236a22ba7aef6110f996104d3
Harlan Cleiton <harlancleiton@gmail.com>
2021-10-23 11:06:59 -0300

    feat: add bootstrap

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,1 +1,24 @@
-console.log('lorem ipsum');
+import express, { Router } from 'express';
+
+import { authApplyRoutes } from './modules/auth';
+
+function applyRoutes(router: Router) {
+  authApplyRoutes(router);
+}
+
+async function bootstrap() {
+  const app = express();
+
+  app.use(express.json());
+
+  const router = Router();
+  applyRoutes(router);
+  app.use(router);
+
+  app.listen(3333, () => {
+    // eslint-disable-next-line no-console
+    console.log('Server is running');
+  });
+}
+
+bootstrap();
