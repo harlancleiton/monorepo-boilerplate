25a9aa63346e968962cbfe1ed32e8a852741bf06
Harlan Cleiton <harlancleiton@gmail.com>
2021-10-26 19:34:58 -0300

    test(server/users): ensure CreateUser calls userRepository.create with correct values

--- packages/server/tests/unit/modules/users/data/usecases/DbCreateUser.spec.ts
+++ packages/server/tests/unit/modules/users/data/usecases/DbCreateUser.spec.ts
@@ -1,13 +1,31 @@
-import { DbCreateUser } from '~/modules/users/data';
+import { DbCreateUser, UserRepository } from '~/modules/users/data';
 
 describe('DbCreateUser', () => {
   let sut: DbCreateUser;
+  let userRepository: UserRepository;
 
   beforeEach(() => {
-    sut = new DbCreateUser();
+    userRepository = { create: jest.fn() };
+    sut = new DbCreateUser(userRepository);
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+  });
+
+  it('should calls usersRepository.create with correct values', async () => {
+    await sut.execute({
+      email: 'mail@mail.com',
+      firstName: 'Lorem',
+      lastName: 'Ipsum',
+      password: '123456'
+    });
+
+    expect(userRepository.create).toBeCalledWith({
+      email: 'mail@mail.com',
+      firstName: 'Lorem',
+      lastName: 'Ipsum',
+      password: '123456'
+    });
   });
 });
