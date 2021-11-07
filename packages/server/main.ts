d3b73c4e0da16fc131bbb361d9fca5046dee170f
Harlan Cleiton <harlancleiton@gmail.com>
2021-11-07 18:39:16 -0300

    chore(server/infra): add AddUsers

--- packages/server/main.ts
+++ packages/server/main.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata';
 
 import dotenv from 'dotenv';
+import { createConnection } from 'typeorm';
 
 import { startServer } from '~/start/server';
 
 async function bootstrap() {
   dotenv.config();
 
+  await createConnection();
   await startServer();
 }
 
