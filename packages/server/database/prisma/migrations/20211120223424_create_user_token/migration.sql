8681fec0c7879815ddeabb6f0cda84fe1736cef3
Harlan Cleiton <harlancleiton@gmail.com>
2021-11-20 20:38:31 -0300

    chore(server/database): add UserToken migration

---
+++ packages/server/database/prisma/migrations/20211120223424_create_user_token/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "UserTokenType" AS ENUM ('access_token', 'refresh_token');
+
+-- CreateTable
+CREATE TABLE "UserToken" (
+    "id" UUID NOT NULL,
+    "token" UUID NOT NULL,
+    "type" "UserTokenType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" UUID NOT NULL,
+
+    CONSTRAINT "UserToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserToken_token_key" ON "UserToken"("token");
+
+-- AddForeignKey
+ALTER TABLE "UserToken" ADD CONSTRAINT "UserToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
