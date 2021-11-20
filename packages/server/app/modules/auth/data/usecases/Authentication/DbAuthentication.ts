4a7c1f17c70524a1b63ea1212cd60e3aaf3793d7
Harlan Cleiton <harlancleiton@gmail.com>
2021-11-20 20:53:50 -0300

    feat(server/users): ensure DbAuthentication call Hash.verify with correct values

--- packages/server/app/modules/auth/data/usecases/Authentication/DbAuthentication.ts
+++ packages/server/app/modules/auth/data/usecases/Authentication/DbAuthentication.ts
@@ -1,3 +1,4 @@
+import { HashContract } from '~/common';
 import {
   Authentication,
   CredentialsModel,
@@ -6,10 +7,15 @@ import {
 import { UserRepository } from '~/modules/users/data';
 
 export class DbAuthentication implements Authentication {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly hash: HashContract
+  ) {}
 
   public async execute(credentials: CredentialsModel): Promise<SessionModel> {
-    await this.userRepository.findOneByEmail(credentials.email);
+    const user = await this.userRepository.findOneByEmail(credentials.email);
+
+    await this.hash.verify(user.password, credentials.password);
 
     return null;
   }
--- packages/server/tests/unit/modules/auth/data/usecases/DbAuthentication.spec.ts
+++ packages/server/tests/unit/modules/auth/data/usecases/DbAuthentication.spec.ts
@@ -1,15 +1,18 @@
+import { HashContract } from '~/common';
 import { DbAuthentication } from '~/modules/auth/data';
 import { UserRepository } from '~/modules/users/data';
 import { factories } from '~/tests/factories';
-import { MockUserRepository } from '~/tests/mocks';
+import { MockHash, MockUserRepository } from '~/tests/mocks';
 
 describe('DbAuthentication', () => {
   let sut: DbAuthentication;
   let userRepository: UserRepository;
+  let hash: HashContract;
 
   beforeEach(() => {
     userRepository = new MockUserRepository();
-    sut = new DbAuthentication(userRepository);
+    hash = new MockHash();
+    sut = new DbAuthentication(userRepository, hash);
   });
 
   it('should be defined', () => {
@@ -24,5 +27,17 @@ describe('DbAuthentication', () => {
     await sut.execute({ email: user.email, password: user.password });
 
     expect(userRepository.findOneByEmail).toBeCalledWith(user.email);
+  });
+
+  it('should be call hash with correct values', async () => {
+    const mockPlainPassword = factories.faker.random.alphaNumeric(8);
+    const user = factories.users.user.build();
+
+    jest.spyOn(userRepository, 'findOneByEmail').mockResolvedValueOnce(user);
+    jest.spyOn(hash, 'verify');
+
+    await sut.execute({ email: user.email, password: mockPlainPassword });
+
+    expect(hash.verify).toBeCalledWith(user.password, mockPlainPassword);
   });
 });
