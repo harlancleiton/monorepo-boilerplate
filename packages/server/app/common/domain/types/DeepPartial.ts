3cb1c7e21e88d9ad435d14b885fe92edb80c6e2d
Harlan Cleiton <harlancleiton@gmail.com>
2021-11-20 13:34:21 -0300

    fix(server/common): changes DeepPartial

--- packages/server/app/common/domain/types/DeepPartial.ts
+++ packages/server/app/common/domain/types/DeepPartial.ts
@@ -1,7 +1,28 @@
-export declare type DeepPartial<TModel> = {
-  [P in keyof TModel]?: TModel[P] extends Array<infer U>
-    ? Array<DeepPartial<U>>
-    : TModel[P] extends ReadonlyArray<infer U>
-    ? ReadonlyArray<DeepPartial<U>>
-    : DeepPartial<TModel[P]> | TModel[P];
+export type DeepPartial<T> = T extends Primitive
+  ? Partial<T>
+  : T extends Array<any>
+  ? T
+  : T extends Set<any>
+  ? T
+  : T extends ReadonlySet<any>
+  ? T
+  : T extends Map<any, any>
+  ? T
+  : T extends ReadonlyMap<any, any>
+  ? T
+  : T extends (...args: any[]) => unknown
+  ? T | undefined
+  : T extends object
+  ? DeepPartialObject<T>
+  : unknown;
+export type Primitive =
+  | null
+  | undefined
+  | string
+  | number
+  | boolean
+  | symbol
+  | bigint;
+export type DeepPartialObject<ObjectType extends object> = {
+  [KeyType in keyof ObjectType]?: DeepPartial<ObjectType[KeyType]>;
 };
