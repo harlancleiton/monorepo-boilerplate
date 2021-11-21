b35b7097c9b22c4f272e8919efec67dc4ac13216
Harlan Cleiton <harlancleiton@gmail.com>
2021-11-21 00:13:08 -0300

    feat(server/auth): ensure DbAuthentication throw UnauthorizedException if password is not match

--- packages/server/app/modules/auth/data/usecases/Authentication/DbAuthentication.ts
+++ packages/server/app/modules/auth/data/usecases/Authentication/DbAuthentication.ts
@@ -15,10 +15,19 @@ export class DbAuthentication implements Authentication {
   public async execute(credentials: CredentialsModel): Promise<SessionModel> {
     const user = await this.userRepository.findOneByEmail(credentials.email);
 
-    if (!user) throw new UnauthorizedException('Email ou senha inválida');
+    if (!user) throw this.getUnauthorizedException();
 
-    await this.hash.verify(user.password, credentials.password);
+    const passwordIsMatch = await this.hash.verify(
+      user.password,
+      credentials.password
+    );
+
+    if (!passwordIsMatch) throw this.getUnauthorizedException();
 
     return null;
+  }
+
+  private getUnauthorizedException(): UnauthorizedException {
+    return new UnauthorizedException('Email ou senha inválida');
   }
 }
