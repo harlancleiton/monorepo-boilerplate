4a1579c4312e5ac293cbd818bf1fe68c7a43ac7f
Harlan Cleiton <harlancleiton@gmail.com>
2021-11-20 22:04:13 -0300

    feat(server/auth): ensure DbAuthentication throw UnauthorizedException if user is undefined

--- packages/server/app/modules/auth/data/usecases/Authentication/DbAuthentication.ts
+++ packages/server/app/modules/auth/data/usecases/Authentication/DbAuthentication.ts
@@ -1,4 +1,4 @@
-import { HashContract } from '~/common';
+import { HashContract, UnauthorizedException } from '~/common';
 import {
   Authentication,
   CredentialsModel,
@@ -14,6 +14,8 @@ export class DbAuthentication implements Authentication {
 
   public async execute(credentials: CredentialsModel): Promise<SessionModel> {
     const user = await this.userRepository.findOneByEmail(credentials.email);
+
+    if (!user) throw new UnauthorizedException('Email ou senha inválida');
 
     await this.hash.verify(user.password, credentials.password);
 
