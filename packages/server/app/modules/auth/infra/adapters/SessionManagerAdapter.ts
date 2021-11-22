9da6fd93dd84cbefeb5960b16b4a1865c0f31714
Harlan Cleiton <harlancleiton@gmail.com>
2021-11-21 23:00:11 -0300

    feat(server/auth): add SessionManagerAdapter

---
+++ packages/server/app/modules/auth/infra/adapters/SessionManagerAdapter.ts
@@ -0,0 +1,34 @@
+import { UserModel } from '~/modules/users/domain';
+
+import {
+  SessionManagerContract,
+  TokenManagerContract,
+  UserTokenRepository
+} from '../../data';
+import { SessionModel, UserTokenType } from '../../domain';
+
+export class SessionManagerAdapter implements SessionManagerContract {
+  constructor(
+    private readonly tokenManager: TokenManagerContract,
+    private readonly userTokenRepository: UserTokenRepository
+  ) {}
+
+  public async create(user: UserModel): Promise<SessionModel> {
+    const accessToken = await this.tokenManager.encode({ sub: user.id });
+
+    const refreshToken = await this.userTokenRepository.create({
+      type: UserTokenType.REFRESH_TOKEN,
+      user
+    });
+
+    return { accessToken, refreshToken: refreshToken.token, user };
+  }
+
+  public async renew(): Promise<SessionModel> {
+    throw new Error('Method not implemented.');
+  }
+
+  public revokeSessions(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
