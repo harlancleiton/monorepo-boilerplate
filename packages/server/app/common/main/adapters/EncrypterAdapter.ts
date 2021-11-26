7ccfb17be9bb1055c24ff50bf6a87f5f3671b5a8
Harlan Cleiton <harlancleiton@gmail.com>
2021-11-25 21:07:09 -0300

    fix(server/common): changes import

--- packages/server/app/common/main/adapters/EncrypterAdapter.ts
+++ packages/server/app/common/main/adapters/EncrypterAdapter.ts
@@ -1,8 +1,8 @@
-import { Aes256CdcEncrypterAdapter } from '~/common/infra';
+import { Aes256CbcEncrypterAdapter } from '~/common/infra';
 import { encrypterConfig } from '~/config/encrypter';
 
 export function makeEncrypterAdapter() {
-  return new Aes256CdcEncrypterAdapter(
+  return new Aes256CbcEncrypterAdapter(
     encrypterConfig.encrypters['aes-256-cbc']
   );
 }
